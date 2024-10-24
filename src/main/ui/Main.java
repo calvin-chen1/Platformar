7a184ce65eb9b4d6bfa7e98c2aaf9aeb09b3cca8
Calvin Chen <cchen555@student.ubc.ca>
2024-10-23 19:32:01 -0700

    Added catch statements and protection for wrong inputs

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,10 +3,13 @@ package ui;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+import java.util.InputMismatchException;
+
 public class Main {
     private static ArrayList<Object[][]> levels = new ArrayList<>();
-    
-    /* REQUIRES: 2D Object array (frame)
+
+    /*
+     * REQUIRES: 2D Object array (frame)
      * MODIFIES: this
      * EFFECTS: adds a 2D frame to levels
      */
@@ -14,9 +17,11 @@ public class Main {
         levels.add(f);
     }
 
-    /* REQUIRES: levels 
-     * EFFECTS: prints out the levels in the levels ArrayList and allows you to select
-     *          one by its corresponding number in the list
+    /*
+     * REQUIRES: levels
+     * EFFECTS: prints out the levels in the levels ArrayList and allows you to
+     * select
+     * one by its corresponding number in the list
      */
     public static void levelsMenu() {
         Scanner in = new Scanner(System.in);
@@ -24,19 +29,24 @@ public class Main {
         for (int i = 0; i < levels.size(); i++) {
             System.out.print("[" + (i + 1) + "] ");
         }
+        int input = 0;
         System.out.println();
-        System.out.println("Select which one you would like to try.");
-        int input = in.nextInt();
-        while (input < 1 || input > levels.size()) {
+        do {
+            try {
+                System.out.println("Select which one you would like to try.");
+                input = in.nextInt();
+            } catch (InputMismatchException e) {
+                in.nextLine();
+            }
             System.out.println("Invalid input. Please choose a number that is listed.");
-            input = in.nextInt();
-        }
+        } while (input < 1 || input > levels.size());
         LevelFrame game = new LevelFrame(levels.get(input - 1));
         game.start();
         in.close();
     }
 
-    /* REQUIRES: LevelFrame
+    /*
+     * REQUIRES: LevelFrame
      * EFFECTS: prints out the menu for all of the options in platformer
      */
     public static void menu() {
@@ -48,33 +58,38 @@ public class Main {
         System.out.println("    [3] View collectibles");
         System.out.println("    [4] Add a random level");
         System.out.println("-------------------------------------------------------");
-        
+
         menuScanner(in);
     }
-    
+
     private static void menuScanner(Scanner in) {
         LevelFrame game = new LevelFrame();
-        int i = in.nextInt();
+        int i = 0;
         do {
-            switch (i) {
-                case 1:
-                    game.start();
-                    break;
-                case 2:
-                    levelsMenu();
-                    break;
-                case 3:
-                    game.viewCollectibles();
-                    break;
-                case 4:
-                    levels.add(new LevelFrame().getFrame());
-                    System.out.println("Added a randomized level to the levels list.");
-                    LevelFrame.clearScreen();
-                    menu();
-                    break;
-                default:
-                    System.out.println("Invalid input. Please try again (1-4).");
-                    i = in.nextInt();
+            try {
+                i = in.nextInt();
+                switch (i) {
+                    case 1:
+                        game.start();
+                        break;
+                    case 2:
+                        levelsMenu();
+                        break;
+                    case 3:
+                        game.viewCollectibles();
+                        break;
+                    case 4:
+                        levels.add(new LevelFrame().getFrame());
+                        LevelFrame.clearScreen();
+                        System.out.println("Added a randomized level to the levels list.");
+                        menu();
+                        break;
+                    default:
+                        System.out.println("Invalid input. Please try again (1-4).");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please try again (1-4).");
+                in.nextLine();
             }
         } while (i < 1 || i > 4);
     }
