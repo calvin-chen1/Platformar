49d8e2129da0de7cb9ddfaf78f0bf5c34a5dfbea
Calvin Chen <cchen555@student.ubc.ca>
2024-10-24 17:43:02 -0700

    Opted for JSONArray instead of JSONObject in JsonWriter Class

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,35 +1,49 @@
 package persistence;
 import ui.LevelFrame;
 
-import org.json.JSONObject;
+import org.json.JSONArray;
 import java.io.*;
 
 public class JsonWriter {
     private PrintWriter writer; // writes to file
     private String destination; // given destination address to write to
 
-    // EFFECTS: constructs writer to write to destination file
+    /* REQUIRES: destination must be a valid address
+     * EFFECTS: destination is set to write to destination file
+     */
     public JsonWriter(String destination) {
         this.destination = destination;
     }
 
-    // MODIFIES: this
-    // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
-    // be opened for writing
+    /* MODIFIES: this
+     * EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
+     *  be opened for writing
+     */
     public void open() throws FileNotFoundException {
         writer = new PrintWriter(new File(destination));
     }
 
-    // MODIFIES: this
-    // EFFECTS: closes writer
+    /* MODIFIES: this
+     * EFFECTS: closes writer
+     */
     public void close() {
         writer.close();
     }
-    
-    // MODIFIES: this
-    // EFFECTS: writes JSON representation of workroom to file
+
+    /* MODIFIES: this
+     * EFFECTS: writes JSON representation of LevelFrame to file
+     */
     public void write(LevelFrame lf) {
-        JSONObject json = lf.toJson();
+        JSONArray json = lf.toJson();
+        writer.print(json.toString(3));
+    }
+
+    /* MODIFIES: this
+     * EFFECTS: writes JSON representation of LevelFrame to file
+     */
+    public void write(Object[][] frame) {
+        LevelFrame lf = new LevelFrame(frame);
+        JSONArray json = lf.toJson();
         writer.print(json.toString(3));
     }
 }
