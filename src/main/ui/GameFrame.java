19ac071d3c0d1401e37f97fbada7ec2c34830bd1
Calvin Chen <cchen555@student.ubc.ca>
2024-11-20 19:09:26 -0800

    Added a legend for GameFrame

--- src/main/ui/GameFrame.java
+++ src/main/ui/GameFrame.java
@@ -11,11 +11,12 @@ import model.Cube;
 import model.Platform;
 
 // constructs a graphical version of the LevelFrame
-public class GameFrame {
+public class GameFrame extends JFrame {
     private static LevelFrame gameFrame; // uses a LevelFrame to help with creating the actual frame
     private Object[][] f;
     private JLabel[][] labels;
-    private JFrame gFrame;
+    private JPanel gPanel;
+    private LegendPanel lPanel;
     private Cube cube;
     private Collectible collectible;
 
@@ -24,15 +25,13 @@ public class GameFrame {
      * EFFECTS: constructs gameFrame and borrows elements from LevelFrame
      */
     public GameFrame() {
+        super("Platformar");
+        gPanel = new JPanel();
+        lPanel = new LegendPanel();
         gameFrame = new LevelFrame();
         f = gameFrame.getFrame();
-        gFrame = new JFrame("Platfomar");
         labels = new JLabel[8][8];
         setLabels();
-    }
-
-    public JFrame getFrame() {
-        return this.gFrame;
     }
 
     private void setLabels() {
@@ -53,19 +52,21 @@ public class GameFrame {
      * EFFECTS: intializes the graphical window display for the frame
      */
     public void initialize() {
-        gFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        gFrame.setPreferredSize(new Dimension(800, 800));
-        gFrame.setLayout(new GridLayout(f.length, f[0].length));
-        gFrame.addKeyListener(new KeyHandler());
-        gFrame.setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gPanel.setPreferredSize(new Dimension(600, 600));
+        addKeyListener(new KeyHandler());
+        setResizable(false);
+        gPanel.setLayout(new GridLayout(f.length, f[0].length));
         for (int i = 0; i < labels.length; i++) {
             for (int j = 0; j < labels[0].length; j++) {
-                gFrame.add(labels[i][j]);
+                gPanel.add(labels[i][j]);
             }
         }
-        gFrame.pack();
-        gFrame.setLocationRelativeTo(null);
-        gFrame.setVisible(true);
+        add(gPanel);
+        add(lPanel, BorderLayout.NORTH);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 
     /*
@@ -104,11 +105,12 @@ public class GameFrame {
     }
 
     /*
-     * A key handler to respond to key events
+     * EFFECTS: creates a key handler to respond to key events
      */
     private class KeyHandler extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
+            lPanel.redirectConsoleOutput();
             int keyCode = e.getKeyCode();
             JLabel c = labels[cube.getX1()][cube.getY1()];
             if (keyCode == KeyEvent.VK_W)
@@ -120,9 +122,18 @@ public class GameFrame {
             else if (keyCode == KeyEvent.VK_A)
                 gameFrame.moveLeft();
             updateBoard(c);
+            lPanel.updateMsg();
+            System.out.flush();
+            System.setOut(System.out);
         }
     }
 
+
+    /*
+     * REQUIRES: keyPressed, initialLabel
+     * EFFECTS: each time a key is pressed, the frame is updated and
+     * checks if the collectible has been collected
+     */
     private void updateBoard(Component initialLabel) {
         JLabel updatedLabel = labels[cube.getX1()][cube.getY1()];
         initialLabel.setBackground(Color.WHITE);
@@ -137,18 +148,21 @@ public class GameFrame {
         updatedLabel.repaint();
     }
 
+    /*
+     * EFFECTS: displays a win popup and prompts the user to replay, and sends 
+     * an event to notify the thread to continue
+     */
     private void win() {
-        JOptionPane.showMessageDialog(gFrame, "You won!");
-        int n = JOptionPane.showConfirmDialog(gFrame,
+        JOptionPane.showMessageDialog(gPanel, "You won!");
+        int n = JOptionPane.showConfirmDialog(gPanel,
                 "Would you like to replay?", "", JOptionPane.YES_NO_OPTION);
 
         if (n == JOptionPane.YES_OPTION) {
             Main.restart = true;
         } else {
-            JOptionPane.showMessageDialog(gFrame, "Thanks for playing!");
+            JOptionPane.showMessageDialog(gPanel, "Thanks for playing!");
             Main.restart = false;
         }
-        gFrame.dispatchEvent(new WindowEvent(gFrame, WindowEvent.WINDOW_CLOSED));
-
+        dispatchEvent(new WindowEvent(getWindows()[0], WindowEvent.WINDOW_CLOSED));
     }
 }
---
+++ src/main/ui/LegendPanel.java
@@ -0,0 +1,49 @@
+package ui;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.Box;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+// constructs the panel displayed on the top of GameFrame
+public class LegendPanel extends JPanel {
+    private static final String GUIDE_TXT = "Blue is you. Red is lava. Yellow is the goal."; // guide text
+    private JLabel msgLbl; // represents the message displayed
+    private ByteArrayOutputStream baos; // the redirected connection from console output
+
+    /*
+     * EFFECTS: creates a JPanel that represents the legend and the corresponding messages 
+     * delivered from LevelFrame
+     */
+    public LegendPanel() {
+        setBackground(new Color(211, 211, 211));
+        msgLbl = new JLabel(GUIDE_TXT);
+        msgLbl.setPreferredSize(new Dimension(250, 30));
+        add(msgLbl);
+        add(Box.createHorizontalStrut(5));
+    }
+
+    /*
+     * EFFECTS: redirects the stream from the console output to a new 
+     * ByteArrayOutputStream, and can take anything printed towards the
+     * console and convert it to a string
+     */
+    public void redirectConsoleOutput() {
+        baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        System.setOut(ps);
+    }
+
+    /* 
+     * EFFECTS: updates msgLbl with the converted information from baos
+     */
+    public void updateMsg() {
+        msgLbl.setText(baos.toString().substring(7));
+        repaint();
+    }
+}
